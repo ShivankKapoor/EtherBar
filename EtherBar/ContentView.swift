074ec081c9ec91029706be31eaa87eb4a54c5060
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-03-03 15:52:20 -0600

    Initial Commit

---
+++ EtherBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EtherBar
+//
+//  Created by Shivank Kapoor on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EtherBar/EtherBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtherBarApp.swift
+//  EtherBar
+//
+//  Created by Shivank Kapoor on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct EtherBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
